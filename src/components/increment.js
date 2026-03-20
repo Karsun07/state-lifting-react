f521c724ae3c08a57ee9f8524f682250840b378a
Kartik Sundriyal <kartiksundriyal2004@gmail.com>
2026-03-20 18:46:41 +0530

    child and parent counter made

---
+++ src/components/increment.js
@@ -0,0 +1,8 @@
+function Increment(props){
+    return (
+        <div>
+            <h1>child count :{props.counts}</h1>
+        </div>
+    )
+}
+export default Increment;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import React from "react";
-import React from "react-dom/client";
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 import Increment from "./components/increment";
 
 function App(){
-    
+    const [count,setCount]=useState(0);
+    return (
+        <>
+        <div>
+        <h1>Parent count:{count}</h1>
+        <button onClick={()=>setCount(count+1)}>Increment</button>
+        </div>
+
+        <Increment counts={count}/>
+
+
+
+        </>
+    )
 }
+const reactRoot=ReactDOM.createRoot(document.getElementById("root"));
+reactRoot.render(<App/>);
